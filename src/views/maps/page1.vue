ad2fd60a1119c6cd84771249e47bce6afa199777
leviheart <15720605398@163.com>
2023-06-14 00:30:32 +0800

    page1添加饼图

--- src/views/maps/page1.vue
+++ src/views/maps/page1.vue
@@ -1,98 +1,134 @@
 <template>
-    <div>
-        <div id="test"></div>
-    </div>
+  <div>
+    <div id="test"></div>
+  </div>
 </template>
-  
+
 <script>
 export default {
-    data() {
-        return {};
-    },
-    mounted() {
-        this.getECharts();
-    },
-    methods: {
-        getECharts() {
-            // 基于准备好的dom，初始化echarts实例
-            var myChart = this.$echarts.init(document.getElementById("test"), null, {
-                // width: 100,
-                height: 500,
-            });
-            let option = {
-                tooltip: {
-                    trigger: 'item'
-                },
-                legend: {
-                    top: '15%',
-                    right: '10%',
-                    orient: 'vertical', // 图例垂直布局
-                    align: 'right', // 图例文字靠右对齐
-                },
-                series: [
-                    {
-                        name: 'Access From',
-                        type: 'pie',
-                        radius: ['40%', '70%'],
-                        avoidLabelOverlap: false,
-                        itemStyle: {
-                            borderRadius: 10,
-                            borderColor: '#fff',
-                            borderWidth: 2
-                        },
-                        label: {
-                            show: false,
-                            position: 'center'
-                        },
-                        emphasis: {
-                            label: {
-                                show: true,
-                                fontSize: 40,
-                                fontWeight: 'bold'
-                            }
-                        },
-                        labelLine: {
-                            show: false
-                        },
-                        data: [
-                            { value: 1048, name: 'Search Engine' },
-                            { value: 735, name: 'Direct' },
-                            { value: 580, name: 'Email' },
-                            { value: 484, name: 'Union Ads' },
-                            { value: 300, name: 'Video Ads' }
-                        ]
-                    }
-                ]
-            };
-
-            option && myChart.setOption(option);
-            myChart.on('click', (params) => {
-                console.log(params);
-                this.changeList(params);
-            });
-            myChart.on('legendselectchanged', (params) => {
-                console.log(params, option);
-                this.changeList(params);
-                //加上这一行
-                myChart.setOption({
-                    legend: { selected: { [params.name]: true } }
-                });
-            });
-            //防抖
-            let timer;
-            window.onresize = function () {
-                if (timer) {
-                    clearTimeout(timer);
-                }
-                timer = setTimeout(() => {
-                    myChart.resize();
-                }, 500);
-            };
+  data() {
+    return {};
+  },
+  mounted() {
+    this.getECharts();
+  },
+  methods: {
+    getECharts() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = this.$echarts.init(document.getElementById("test"), null, {
+        // width: 100,
+        height: 500,
+      });
+      let option = {
+        tooltip: {
+          trigger: "item",
         },
-        changeList(params) {
+        legend: {
+          top: "15%",
+          right: "10%",
+          orient: "vertical", // 图例垂直布局
+          align: "right", // 图例文字靠右对齐
+        },
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            radius: ["40%", "70%"],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: "#fff",
+              borderWidth: 2,
+            },
+            label: {
+              show: false,
+              position: "center",
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 40,
+                fontWeight: "bold",
+              },
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              { value: 1048, name: "Search Engine" },
+              { value: 735, name: "Direct" },
+              { value: 580, name: "Email" },
+              { value: 484, name: "Union Ads" },
+              { value: 300, name: "Video Ads" },
+            ],
+          },
+        ],
+      };
+
+      //写一个echarts饼图option，变量名为option1
+      let option1 = {
+        title: {
+          text: "饼图",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["盒学", "小学", "创业", "大学", "其他"],
+        },
+        series: [
+          {
+            name: "访问次数",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "盒学" },
+              { value: 310, name: "小学" },
+              { value: 234, name: "创业" },
+              { value: 135, name: "大学" },
+              { value: 1548, name: "其他" },
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      };
+      option1 && myChart.setOption(option1);
+      myChart.on("click", (params) => {
+        console.log(params);
+        this.changeList(params);
+      });
+      myChart.on("legendselectchanged", (params) => {
+        console.log(params, option);
+        this.changeList(params);
+        //加上这一行
+        myChart.setOption({
+          legend: { selected: { [params.name]: true } },
+        });
+      });
+      //防抖
+      let timer;
+      window.onresize = function() {
+        if (timer) {
+          clearTimeout(timer);
         }
+        timer = setTimeout(() => {
+          myChart.resize();
+        }, 500);
+      };
     },
+    changeList(params) {},
+  },
 };
 </script>
-  
+
 <style></style>
