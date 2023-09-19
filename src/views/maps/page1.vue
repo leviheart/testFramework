44c7cd55ac2b55fcdfea60c86040464aab1e4071
leviheart <15720605398@163.com>
2023-09-19 20:29:15 +0800

    多图表测试

--- src/views/maps/page1.vue
+++ src/views/maps/page1.vue
@@ -1,141 +1,49 @@
 <template>
   <div>
     <el-button type="primary" @click="openDialog">打开弹框</el-button>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="90%">
-      <div id="test"></div>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="90%"
+      @close="handleClose"
+    >
+      <el-button type="primary" @click="onVisble">更改显示</el-button>
+      <charDialog ref="charDialog1" :showEcharts="showEcharts1"></charDialog>
+      <charDialog ref="charDialog2" :showEcharts="showEcharts2"></charDialog>
+      <charDialog ref="charDialog3" :showEcharts="showEcharts3"></charDialog>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import charDialog from "../components/dialog.vue";
 export default {
   data() {
     return {
       dialogVisible: false,
+      showEcharts1: true,
+      showEcharts2: true,
+      showEcharts3: true,
     };
+  },
+  components: {
+    charDialog,
   },
   mounted() {},
   methods: {
     openDialog() {
       this.dialogVisible = true;
       this.$nextTick(() => {
-        this.getECharts();
+        this.$refs.charDialog1.getECharts();
+        this.$refs.charDialog2.getECharts();
+        this.$refs.charDialog3.getECharts();
       });
     },
-    getECharts() {
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = this.$echarts.init(document.getElementById("test"), null, {
-        renderer: "svg",
-        height: 500,
-      });
-      let option = {
-        tooltip: {
-          trigger: "item",
-        },
-        legend: {
-          top: "15%",
-          right: "10%",
-          orient: "vertical", // 图例垂直布局
-          align: "right", // 图例文字靠右对齐
-        },
-        series: [
-          {
-            name: "Access From",
-            type: "pie",
-            radius: ["40%", "70%"],
-            avoidLabelOverlap: false,
-            itemStyle: {
-              borderRadius: 10,
-              borderColor: "#fff",
-              borderWidth: 2,
-            },
-            label: {
-              show: false,
-              position: "center",
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: 40,
-                fontWeight: "bold",
-              },
-            },
-            labelLine: {
-              show: false,
-            },
-            data: [
-              { value: 1048, name: "Search Engine" },
-              { value: 735, name: "Direct" },
-              { value: 580, name: "Email" },
-              { value: 484, name: "Union Ads" },
-              { value: 300, name: "Video Ads" },
-            ],
-          },
-        ],
-      };
-
-      //写一个echarts饼图option，变量名为option1
-      let option1 = {
-        title: {
-          text: "饼图",
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)",
-        },
-        legend: {
-          orient: "vertical",
-          left: "left",
-          data: ["盒学", "小学", "创业", "大学", "其他"],
-        },
-        series: [
-          {
-            name: "访问次数",
-            type: "pie",
-            radius: "55%",
-            center: ["50%", "60%"],
-            data: [
-              { value: 335, name: "盒学" },
-              { value: 310, name: "小学" },
-              { value: 234, name: "创业" },
-              { value: 135, name: "大学" },
-              { value: 1548, name: "其他" },
-            ],
-            itemStyle: {
-              emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: "rgba(0, 0, 0, 0.5)",
-              },
-            },
-          },
-        ],
-      };
-      option1 && myChart.setOption(option1);
-      myChart.on("click", (params) => {
-        console.log(params);
-        this.changeList(params);
-      });
-      myChart.on("legendselectchanged", (params) => {
-        console.log(params, option);
-        this.changeList(params);
-        //加上这一行
-        myChart.setOption({
-          legend: { selected: { [params.name]: true } },
-        });
-      });
-      //防抖
-      let timer;
-      window.onresize = function() {
-        if (timer) {
-          clearTimeout(timer);
-        }
-        timer = setTimeout(() => {
-          myChart.resize();
-        }, 500);
-      };
+    onVisble() {
+      this.showEcharts1 = false;
+      this.showEcharts2 = false;
+      this.showEcharts3 = false;
     },
-    changeList(params) {},
   },
 };
 </script>
