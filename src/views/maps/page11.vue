e087c8b31554a99adba22606ece5eec98c610d43
leviheart <15720605398@163.com>
2024-10-14 21:54:50 +0800

    页面11

--- src/views/maps/page11.vue
+++ src/views/maps/page11.vue
@@ -17,50 +17,68 @@ export default {
       center: [118.791036, 32.065599], // 江苏省大致中心点坐标
       zoom: 6, // 初始缩放级别
     });
-    this.init();
+    const self = this;
+    map.on("load", function() {
+      self.init(map);
+    });
   },
   methods: {
-    init() {
-      var polygon1 = turf.polygon(
-        [
-          [
-            [128, -26],
-            [141, -26],
-            [141, -21],
-            [128, -21],
-            [128, -26],
-          ],
-        ],
-        {
-          fill: "#F00",
-          "fill-opacity": 0.1,
-        }
-      );
-      var polygon2 = turf.polygon(
-        [
-          [
-            [126, -28],
-            [140, -28],
-            [140, -20],
-            [126, -20],
-            [126, -28],
-          ],
-        ],
-        {
-          fill: "#00F",
-          "fill-opacity": 0.1,
-        }
-      );
+    init(map) {
+      var line = turf.lineString([
+        [0, 0],
+        [22, 6],
+        [123, 7],
+        [124, 38],
+        [124, 69],
+      ]);
 
-      var difference = turf.difference(
-        turf.featureCollection([polygon1, polygon2])
-      );
-      console.log(difference,"difference");
-      
+      var bbox = turf.bbox(line);
+      var center = turf.point([(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]);
+      var closestPoint = turf.nearestPointOnLine(line, center, { units: "miles" });
+      console.log(bbox,line,center,closestPoint,(bbox[0] + bbox[2]) / 2);
+
+      // 将线要素添加到地图上
+      map.addSource("line-source", {
+        type: "geojson",
+        data: line,
+      });
+
+      map.addLayer({
+        id: "line-layer",
+        source: "line-source",
+        type: "line",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#888",
+          "line-width": 3,
+        },
+      });
+
+      // 将中心点要素添加到地图上
+      map.addSource("center-source", {
+        type: "geojson",
+        data: closestPoint,
+      });
+      // 加载车辆图标
+      let imageUrl = require("@/assets/image/骑车.jpg");
+      map.loadImage(imageUrl, function(error, image) {
+        map.addImage("ride", image);
+      });
+      map.addLayer({
+        id: "center-layer",
+        source: "center-source",
+        type: "symbol",
+        layout: {
+          "icon-image": "ride", // 替换为你的图标
+          "icon-size": 0.5,
+        },
+      });
     },
   },
-}
-
+};
 </script>
 
 <style scoped>
